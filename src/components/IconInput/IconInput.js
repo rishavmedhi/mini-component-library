109f8e84030eb9981077a2ff8d11cea3790d7d1b
Rishav Medhi <rishav164@gmail.com>
2022-02-14 03:10:33 +0530

    IconInput my solution

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,66 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const SIZE = {
+    "small": {
+      fontSize: 14,
+      iconSize: 16,
+      paddingLeft: 24
+    },
+    "large": {
+      fontSize: 18,
+      iconSize: 24,
+      paddingLeft: 36
+    }
+  }
+  return <Wrapper>
+          <NativeInput style={
+            {
+              "--width": width+"px", 
+              "--fontSize": (SIZE[size].fontSize/16)+"rem",
+              "--paddingLeft":SIZE[size].paddingLeft+"px"
+            }} 
+            placeholder={placeholder} />
+          <IconWrapper>
+            <Icon id={icon} strokeWidth={1} size={SIZE[size].iconSize} />
+          </IconWrapper>
+          <VisuallyHidden>{label}</VisuallyHidden>
+        </Wrapper>
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  border-bottom: 1px solid ${COLORS.black};
+  width: max-content;
+`;
+
+const NativeInput = styled.input`
+  border: none;
+  padding-left: var(--paddingLeft);
+  width: var(--width);
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  outline-offset: 4px;
+  font-size: var(--fontSize);
+  &:hover{
+    color: ${COLORS.black};
+  }
+  &::placeholder{
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  margin: auto;
+  color: ${COLORS.gray700};
+  ${NativeInput}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default IconInput;

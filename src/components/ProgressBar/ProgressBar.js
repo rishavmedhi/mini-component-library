9d2779cab92ef9b264ddd96adddb0e29fe6b9591
Rishav Medhi <rishav164@gmail.com>
2022-02-13 16:42:35 +0530

    progress bar component solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,61 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const style={
+    "small": {
+      "height": 8,
+      "radius": 4,
+      "padding": 0,
+    },
+    "medium": {
+      "height": 12,
+      "radius": 4,
+      "padding": 0,
+    },
+    "large": {
+      "height": 16,
+      "radius": 8,
+      "padding": 4,
+    }
+  }
+  return <Wrapper
+    style={{
+      "--padding": style[size].padding+"px",
+      "--radius": style[size].radius+"px"
+    }} 
+    role="progressbar" 
+    aria-valuenow={value} 
+    aria-valuemin="0" 
+    aria-valuemax="100">
+      <BarWrapper>
+        <Bar style={{
+          "--width": value+"%",
+          "--height": style[size].height+"px"
+        }}/>
+      </BarWrapper>
+      <VisuallyHidden>{value + " %"}</VisuallyHidden>
+    </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  border-radius: 4px;
+  background-color: ${COLORS.transparentGray15};
+  padding: var(--padding);
+  border-radius: var(--radius);
+   /* follows the curving at edges at high values */
+   overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  background-color: ${COLORS.primary};
+  height: var(--height);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* for curving at the edges for large variant */
+  overflow: hidden;
+`;
 
 export default ProgressBar;
